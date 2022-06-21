d7e4c19d9f43979d25ca3a359c3df8836ca4117b
Jakob Schödl <jschoedl04@gmail.com>
2022-06-21 15:27:16 +0200

    add questions, load question buttons dynamically

--- src/QuestionButtons.jsx
+++ src/QuestionButtons.jsx
@@ -1,51 +1,27 @@
 import React from 'react';
 import Button from "./Button";
+import questions from "./assets/questions.json"
 
 function QuestionButtons() {
+    const questionsPerCategory = Math.max(
+        ...Object.values(questions).map(categoryQuestions => Object.keys(categoryQuestions).length)
+    )
+
+    let categoryHeaders = [Object.keys(questions).map(categoryName => <h2 key={categoryName}>{categoryName}</h2>)]
+
+    let questionRows = []
+    for (let questionIndex = 0; questionIndex < questionsPerCategory; questionIndex++) {
+        let questionButtons = []
+        for (let [categoryName, categoryQuestions] of Object.entries(questions)) {
+            questionButtons.push(<Button key={categoryName} value={Object.keys(categoryQuestions)[questionIndex]}/>)
+        }
+        questionRows.push(<div className="container" key={questionIndex}>{questionButtons}</div>)
+    }
+
     return (
-        <div className="container flex-2" id="question-buttons">
-            <div className="container">
-                <h2>Test</h2>
-                <h2>Test</h2>
-                <h2>Test</h2>
-                <h2>Test</h2>
-                <h2>Test</h2>
-            </div>
-            <div className="container">
-                <Button value={200}/>
-                <Button value={200}/>
-                <Button value={200}/>
-                <Button value={200}/>
-                <Button value={200}/>
-            </div>
-            <div className="container">
-                <Button value={200}/>
-                <Button value={200}/>
-                <Button value={200}/>
-                <Button value={200}/>
-                <Button value={200}/>
-            </div>
-            <div className="container">
-                <Button value={200}/>
-                <Button value={200}/>
-                <Button value={200}/>
-                <Button value={200}/>
-                <Button value={200}/>
-            </div>
-            <div className="container">
-                <Button value={200}/>
-                <Button value={200}/>
-                <Button value={200}/>
-                <Button value={200}/>
-                <Button value={200}/>
-            </div>
-            <div className="container">
-                <Button value={200}/>
-                <Button value={200}/>
-                <Button value={200}/>
-                <Button value={200}/>
-                <Button value={200}/>
-            </div>
+        <div className="container flex-3" id="question-buttons">
+            <div className="container">{categoryHeaders}</div>
+            {questionRows}
         </div>
     );
 }
