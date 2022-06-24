dbc18abaa059c64c75cee376d0a185760fc1ddb5
Jakob Schödl <jschoedl04@gmail.com>
2022-06-24 10:45:54 +0200

    implement correct / wrong answers

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,16 @@
 import QuestionButtons from "./QuestionButtons";
 import TeamScores from "./TeamScores";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import Question from "./Question";
-import {useKeyPress} from "./lib/helpers";
 
 function App() {
     const [displayedQuestion, setDisplayedQuestion] = useState({})
-    const [answerVisible, setAnswerVisible] = useState(false)
-    const spacePressed = useKeyPress(" ")
-
-    useEffect(() => {
-        if (spacePressed) {
-            if (!answerVisible)
-                setAnswerVisible(true)
-            else {
-                setDisplayedQuestion({})
-                setAnswerVisible(false)
-            }
-        }
-    }, [spacePressed])
 
     return (
         <>
-            <Question displayedQuestion={displayedQuestion} answerVisible={answerVisible}/>
+            {Object.keys(displayedQuestion).length
+                ? <Question displayedQuestion={displayedQuestion} setDisplayedQuestion={setDisplayedQuestion}/>
+                : <></>}
             <QuestionButtons setDisplayedQuestion={setDisplayedQuestion}/>
             <TeamScores/>
         </>
--- src/Question.jsx
+++ src/Question.jsx
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useKeyPress} from "./lib/helpers";
 
-function Question({displayedQuestion, answerVisible}) {
+function Question({displayedQuestion, setDisplayedQuestion}) {
     const {category, value, question} = displayedQuestion;
     const [questionText, answerText] = question || ["", ""]
+
+    const [backgroundColor, setBackgroundColor] = useState("")
+    const [answerVisible, setAnswerVisible] = useState(false)
+    const [scoreChange, setScoreChange] = useState(0)
+
+    const spacePressed = useKeyPress(" ")
+    const arrowUpPressed = useKeyPress("ArrowUp")
+    const arrowDownPressed = useKeyPress("ArrowDown")
+
+    useEffect(() => {
+        if (spacePressed) {
+            console.log(answerVisible)
+            if (!answerVisible && !backgroundColor)
+                setAnswerVisible(true)
+            else if (backgroundColor) {
+                setAnswerVisible(false)
+                setDisplayedQuestion({})
+                setBackgroundColor("")
+            }
+        }
+    }, [backgroundColor, setDisplayedQuestion, spacePressed])
+
+    useEffect(() => {
+        if (arrowUpPressed) {
+            setBackgroundColor("#cefdbc")
+            setScoreChange(value)
+        }
+    }, [arrowUpPressed, value])
+
+    useEffect(() => {
+        if (arrowDownPressed) {
+            setBackgroundColor("#fdbcbc")
+            setScoreChange(-value)
+        }
+    }, [arrowDownPressed, value])
 
 
     return (
         Object.keys(displayedQuestion).length
-            ? <div id="question">
+            ? <div id="question" style={{backgroundColor: backgroundColor}}>
                 <h1>{category} · {value}€</h1>
                 <p>{questionText}</p>
                 {answerVisible ? <p>{answerText}</p> : <></>}
