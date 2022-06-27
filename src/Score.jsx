744919af1d7673c959341b7631214e4d724baeec
Jakob Schödl <jschoedl04@gmail.com>
2022-06-27 16:15:40 +0200

    count animation

--- src/Score.jsx
+++ src/Score.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
+import CountUp from "react-countup";
 
 function Score({teamName, score, active}) {
+    const [lastScore, setLastScore] = useState(0)
+
     return (
         <>
             <h3 className={active ? "" : " inactive"}>{teamName}</h3>
-            <p className={"score" + (active ? "" : " inactive")}>{score}</p>
+            <p className={"score" + (active ? "" : " inactive")}>
+                <CountUp start={lastScore} end={score} duration={1} onEnd={() => setLastScore(score)}/>
+            </p>
         </>
     );
 }
